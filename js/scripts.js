1e9424e7f173aca41eae7851c9ff43911709cc5c
Mark Avila <markjustineavila@gmail.com>
2021-01-31 19:57:25 +0800

    string fixes for contents.js; added slider functionality for dynamic content

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,44 @@ import UI from './ui.js';
 import Content from './content.js';
 
 class App {
-  constructor() {}
+  initialRender = false;
+
+  constructor(isMobile, cnt) {
+    this.isMobile = isMobile;
+    this.cnt = cnt;
+    this.ui = new UI();
+  }
 
   init() {
-    const ui = new UI();
+    if (!this.initialRender) {
+      this.render();
+      this.initialRender = true;
+    }
+
+    this.ui.sliderLeft.addEventListener('click', () => {
+      if (this.cnt !== 0) {
+        this.cnt = this.cnt - 1;
+        this.render();
+      }
+    });
+    this.ui.sliderRight.addEventListener('click', () => {
+      if (this.cnt < 2) {
+        this.cnt = this.cnt + 1;
+        this.render();
+      }
+    });
+  }
+
+  render() {
+    const content = new Content(this.isMobile, this.cnt);
+    const contObject = content.getContent();
+    this.ui.h1.innerText = contObject.title;
+    this.ui.mainP.innerText = contObject.description;
+    this.ui.hero.src = `/images/${contObject.picture}`;
   }
 }
 
-const app = new App();
+const isMobile = window.innerWidth <= 1439 ? true : false;
+
+const app = new App(isMobile, 0);
 app.init();
