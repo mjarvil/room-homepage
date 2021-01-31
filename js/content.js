2e5457632fc6f7e028757a52779fda1f873f74c3
Mark Avila <markjustineavila@gmail.com>
2021-01-31 18:21:55 +0800

    imported elements and set type to module

---
+++ js/content.js
@@ -0,0 +1,9 @@
+class Content {
+  constructor(picture, title, description) {
+    this.picture = picture;
+    this.title = title;
+    this.description = description;
+  }
+}
+
+export default Content;
