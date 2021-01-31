efff3a67dcdd0dcf5906389af786f93db7bdfabe
Mark Avila <markjustineavila@gmail.com>
2021-01-31 18:39:09 +0800

    moved ui controller

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,5 @@
 import Content from './content.js';
 
-const sliderLeft = document.querySelector('.slider-left');
-const sliderRight = document.querySelector('.slider-right');
-const hero = document.querySelector('.hero img');
-const h1 = document.querySelector('h1');
-const mainP = document.querySelector('.main-content p');
-
 sliderLeft.addEventListener('click', () => {
   {
     console.log(`left is clicked`);
--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,11 @@
-class UI {}
+class UI {
+  constructor() {
+    this.sliderLeft = document.querySelector('.slider-left');
+    this.sliderRight = document.querySelector('.slider-right');
+    this.hero = document.querySelector('.hero img');
+    this.h1 = document.querySelector('h1');
+    this.mainP = document.querySelector('.main-content p');
+  }
+}
 
 export default UI;
