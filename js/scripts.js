da59654ae4d8c89a918b2ebfe383e8c9fbd9fcb6
Mark Avila <markjustineavila@gmail.com>
2021-01-31 16:12:16 +0800

    added event handler for slider

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+const sliderLeft = document.querySelector('.slider-left');
+const sliderRight = document.querySelector('.slider-right');
+
+sliderLeft.addEventListener('click', () => {
+  {
+    console.log(`left is clicked`);
+  }
+});
+
+sliderRight.addEventListener('click', () => {
+  {
+    console.log(`right is clicked`);
+  }
+});
